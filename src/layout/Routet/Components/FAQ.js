1c1f0fd10a798fb5e7ff9ce77cbe18c7095966c1
saidrafi11 <said.rafi@outlook.com>
2022-10-27 22:58:40 +0600

    faq final

--- src/layout/Routet/Components/FAQ.js
+++ src/layout/Routet/Components/FAQ.js
@@ -8,7 +8,7 @@ const FAQ = () => {
   </div>
   <div class="card-body">
     <blockquote class="blockquote mb-0">
-      
+      <p>A well-known quote, contained in a blockquote element.</p>
       <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
     </blockquote>
   </div>
